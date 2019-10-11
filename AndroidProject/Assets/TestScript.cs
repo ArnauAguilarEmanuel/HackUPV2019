2186968d509acd8913f9ac62070c392ff8235fbe
Arnau Aguilar <arnau.aguilar@enti.cat>
2019-10-11 17:08:41 +0200

    Initial Comit

---
+++ AndroidProject/Assets/TestScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void changeText()
+    {
+        transform.GetChild(0).GetComponent<Text>().text = "IT WORKED";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
