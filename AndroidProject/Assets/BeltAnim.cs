513ae005702a7623f1cc883815fbf034391672b6
MarcSolis <marc.solis@enti.cat>
2019-10-13 08:28:35 +0200

    Belt Animation

---
+++ AndroidProject/Assets/BeltAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltAnim : MonoBehaviour
+{
+
+    public List<GameObject> myBelt;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Vector3 origialPosition;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject o in myBelt)
+        {
+            o.transform.position += new Vector3(1,0,0) * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Belt")
+        {
+            other.transform.position = origialPosition;
+        }
+    }
+}
